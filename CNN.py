007c79c31111652647430e237f6d64000560ae37
Eweaa <yousefmohamed.ym2001@gmail.com>
2023-04-20 02:49:09 +0200

    Added Threshold Evaluation Part
    There is an error while calculating the mean (average).

--- CNN.py
+++ CNN.py
@@ -84,43 +84,43 @@ plt.show()
 import numpy as np
 from keras.preprocessing import image
 from keras.utils import load_img
-
-imgPath = r"C:\Users\Eweaa\Desktop\Python\CNN3\Dataset\TEST\Label 0\img1429.jpg"
-imgPath2 = r"C:\Users\Eweaa\Desktop\Python\CNN3\Dataset\TEST\Label 1\img1364.jpg"
-imgPath3 = r"C:\Users\Eweaa\Desktop\Python\CNN3\Dataset\TEST\Label 2\img1068.jpg"
+#
+# imgPath = r"C:\Users\Eweaa\Desktop\Python\CNN3\Dataset\TEST\Label 0\img1429.jpg"
+# imgPath2 = r"C:\Users\Eweaa\Desktop\Python\CNN3\Dataset\TEST\Label 1\img1364.jpg"
+# imgPath3 = r"C:\Users\Eweaa\Desktop\Python\CNN3\Dataset\TEST\Label 2\img1068.jpg"
 
 # img = image.load_img(imgPath, target_size=(imgWidth, imgHeight))
-img = keras.utils.load_img(imgPath, target_size=(imgWidth, imgHeight))
-img2 = keras.utils.load_img(imgPath2, target_size=(imgWidth, imgHeight))
-img3 = keras.utils.load_img(imgPath3, target_size=(imgWidth, imgHeight))
-# img2 = image.load_img(imgPath2, target_size=(imgWidth, imgHeight))
-
-plt.imshow(img)
-plt.show()
-
-img = keras.utils.img_to_array(img)
-x = np.expand_dims(img, axis=0) * 1. / 255
-score = model.predict(x)
-print('first degree burn ', score)
+# img = keras.utils.load_img(imgPath, target_size=(imgWidth, imgHeight))
+# img2 = keras.utils.load_img(imgPath2, target_size=(imgWidth, imgHeight))
+# img3 = keras.utils.load_img(imgPath3, target_size=(imgWidth, imgHeight))
+# # img2 = image.load_img(imgPath2, target_size=(imgWidth, imgHeight))
+#
+# plt.imshow(img)
+# plt.show()
+#
+# img = keras.utils.img_to_array(img)
+# x = np.expand_dims(img, axis=0) * 1. / 255
+# score1 = model.predict(x)
+# print('first degree burn ', score1)
 
 # print('Predicted: ', score, 'Chest X-ray' if score < 0.5 else 'Abd x-ray')
 
-plt.imshow(img2)
-plt.show()
-
-img2 = keras.utils.img_to_array(img2)
-x = np.expand_dims(img2, axis=0) * 1. / 255
-score2 = model.predict(x)
-print('second degree burn ', score2)
+# plt.imshow(img2)
+# plt.show()
+#
+# img2 = keras.utils.img_to_array(img2)
+# x = np.expand_dims(img2, axis=0) * 1. / 255
+# score2 = model.predict(x)
+# print('second degree burn ', score2)
 # print('Predicted: ', score2, 'Chest X-ray' if score2 < 0.5 else 'Abd x-ray')
 
-img3 = keras.utils.img_to_array(img3)
-x = np.expand_dims(img2, axis=0) * 1. / 255
-score3 = model.predict(x)
-print('third degree burn ', score3)
-
-plt.imshow(img3)
-plt.show()
+# img3 = keras.utils.img_to_array(img3)
+# x = np.expand_dims(img2, axis=0) * 1. / 255
+# score3 = model.predict(x)
+# print('third degree burn ', score3)
+#
+# plt.imshow(img3)
+# plt.show()
 
 # print('Predicted: ', score2, 'Chest X-ray' if score2 < 0.5 else 'Abd x-ray')
 
@@ -129,3 +129,20 @@ plt.show()
 #
 # for i in range(1440):
 #     labelImgPath =
+
+import shutil
+from statistics import mean
+from os.path import exists
+arr = []
+for x in range(1440):
+    if exists(f'./Dataset/TRAIN/Label 0/img{x}.jpg'):
+        img = keras.utils.load_img(r'./Dataset/TRAIN/Label 0/img'+str(x)+'.jpg', target_size=(imgWidth, imgHeight))
+        img = keras.utils.img_to_array(img)
+        x = np.expand_dims(img, axis=0) * 1. / 255
+        score = model.predict(x)
+        arr.append(score)
+    else:
+        print('does not exist')
+print(arr)
+theMean = mean(arr)
+print('the threshold is ' + str(theMean))
